4bffad2bf53426a88b971ec4ea1ea6616aec4c16
Sridhar Nuvusetty <sridhar.nuvusetty@nordicsemi.no>
2022-06-22 18:24:13 +0530

    Fix RPU_SLEEP_SUPPORT compilation issue ..

--- drivers/wifi/nrf700x/hw_if/hal/inc/hal_api.h
+++ drivers/wifi/nrf700x/hw_if/hal/inc/hal_api.h
@@ -133,6 +133,5 @@ enum wifi_nrf_status wifi_nrf_hal_fw_chk_boot(struct wifi_nrf_hal_dev_ctx *hal_c
 #ifdef RPU_SLEEP_SUPPORT
 enum wifi_nrf_status hal_rpu_ps_wake(struct wifi_nrf_hal_dev_ctx *hal_dev_ctx);
 
-void hal_rpu_ps_sleep(unsigned long data);
 #endif
 #endif /* __HAL_API_H__ */
