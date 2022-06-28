e9635d64e7ae7b553740e969443f6f2bddcd2a90
Krishna T <krishna.t@nordicsemi.no>
2022-06-28 13:43:04 +0530

    wifi: Use proper API to set frequency
    Use the clock divider API to set the frequency, rather than using
    internal methods.
    
    Signed-off-by: Krishna T <krishna.t@nordicsemi.no>

--- samples/wifi/shell/src/main.c
+++ samples/wifi/shell/src/main.c
@@ -6,15 +6,14 @@
 
 #include <zephyr.h>
 #include <sys/printk.h>
+#include <nrfx_clock.h>
+
 
 void main(void)
 {
 #ifdef CONFIG_BOARD_NRF5340DK_NRF5340_CPUAPP
-#define __SYSTEM_CLOCK_MAX      (128000000UL)
-	/* Use CPU freqyency 128MHz i.e., 128MHz/Div1 */
-	*((volatile uint32_t *)0x50039530ul) = 0xBEEF0044ul;
-	NRF_CLOCK_S->HFCLKCTRL = CLOCK_HFCLKCTRL_HCLK_Div1 << CLOCK_HFCLKCTRL_HCLK_Pos;
-	SystemCoreClockUpdate();
+	// For now hardcode to 128MHz
+	nrfx_clock_divider_set(NRF_CLOCK_DOMAIN_HFCLK, NRF_CLOCK_HFCLK_DIV_1);
 #endif
-	printk("Starting %s with CPU frequency: %d\n", CONFIG_BOARD, SystemCoreClock);
+	printf("Starting %s with CPU frequency: %d MHz\n", CONFIG_BOARD, SystemCoreClock/MHZ(1));
 }
