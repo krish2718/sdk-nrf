1f05117431ec3a5f9c9af81e85fc133c9054c18a
Chaitanya Tata <Chaitanya.Tata@nordicsemi.no>
2024-11-13 17:10:46 +0530

    samples: net: mqtt: Configure extended sleep
    Use extended sleep to reproduce a customer issue.
    
    Signed-off-by: Chaitanya Tata <Chaitanya.Tata@nordicsemi.no>

--- samples/net/mqtt/src/modules/network/network.c
+++ samples/net/mqtt/src/modules/network/network.c
@@ -9,6 +9,7 @@
 #include <zephyr/zbus/zbus.h>
 #include <zephyr/net/conn_mgr_connectivity.h>
 #include <zephyr/net/conn_mgr_monitor.h>
+#include <zephyr/net/wifi_mgmt.h>
 
 #include "message_channel.h"
 
@@ -64,6 +65,49 @@ static void connectivity_event_handler(struct net_mgmt_event_callback *cb,
 	}
 }
 
+static int lock_core_wifi_config_power_save(void)
+{
+	struct net_if *iface = net_if_get_default();
+	struct wifi_ps_params params = { 0 };
+
+	params.type = WIFI_PS_PARAM_LISTEN_INTERVAL;
+	params.listen_interval = 10;
+
+	if (net_mgmt(NET_REQUEST_WIFI_PS, iface, &params, sizeof(params))) {
+		if (params.fail_reason == WIFI_PS_PARAM_LISTEN_INTERVAL_RANGE_INVALID) {
+			LOG_ERR("Setting listen interval failed. Reason %s",
+				wifi_ps_get_config_err_code_str(params.fail_reason));
+			LOG_ERR("Hardware support valid range : 3 - 65535");
+		} else {
+			LOG_ERR("Setting listen interval failed. Reason %s",
+				wifi_ps_get_config_err_code_str(params.fail_reason));
+		}
+		return -ENOEXEC;
+	}
+	LOG_INF("Listen interval %hu", params.listen_interval);
+
+	params.type = WIFI_PS_PARAM_WAKEUP_MODE;
+	if (params.listen_interval == 0) {
+		params.wakeup_mode = WIFI_PS_WAKEUP_MODE_DTIM;
+	} else {
+		params.wakeup_mode = WIFI_PS_WAKEUP_MODE_LISTEN_INTERVAL;
+	}
+
+	if (net_mgmt(NET_REQUEST_WIFI_PS, iface, &params, sizeof(params))) {
+		LOG_ERR("Setting PS wake up mode to %s failed..Reason %s",
+			params.wakeup_mode ? "Listen interval" : "DTIM interval",
+			wifi_ps_get_config_err_code_str(params.fail_reason));
+		return -ENOEXEC;
+	}
+
+	LOG_INF("Wifi power save mode set to %s",
+		params.wakeup_mode ? "Listen interval" : "DTIM interval");
+
+	LOG_INF("Wifi power save configs setup success");
+
+	return 0;
+};
+
 static void network_task(void)
 {
 	int err;
@@ -75,6 +119,11 @@ static void network_task(void)
 	/* Setup handler for Zephyr NET Connection Manager Connectivity layer. */
 	net_mgmt_init_event_callback(&conn_cb, connectivity_event_handler, CONN_LAYER_EVENT_MASK);
 	net_mgmt_add_event_callback(&conn_cb);
+
+	/* Wait for Wi-Fi to be up */
+	k_sleep(K_SECONDS(2));
+
+	lock_core_wifi_config_power_save();
 
 	/* Connecting to the configured connectivity layer.
 	 * Wi-Fi or LTE depending on the board that the sample was built for.
